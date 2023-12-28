8edd7723dd8d27c96053c61243d818dd7650130e
Arjun-1111 <contactme.xyz1@gmail.com>
2023-12-28 11:04:47 +0530

    added necessary packages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect(process.env.MONGO_CONNECTION_STRING as string);
 
 app.get("/api/test", (req: Request, res: Response) => {
   res.json("hello from api");
